c51769d2dce7e2b6c99a4e6cfdb0771aeb273271
Tedi <tedi6410@gmail.com>
2014-12-20 15:25:51 +0800

    chapter 3 finished

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from mysite.views import hello
+
+# Remember to import the functions
+from mysite.views import hello, current_datetime, hours_ahead
 
 urlpatterns = patterns('',
     # Examples:
@@ -10,4 +12,8 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     
     url(r'^hello/$', hello),
+    url(r'^time/$' , current_datetime),
+
+    # Using parentheses to save the number
+    url(r'^time/plus/(\d{1,2})/$', hours_ahead),
 )
